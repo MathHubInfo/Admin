339e2d75156672543735969ed2ec921aed889f01
Tom Wiesing <tkw01536@gmail.com>
2018-04-28 11:53:41 +0200

    Add minimal Django Project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Admin
-An authentication server for the admin parts of MathHub
+
+A tiny authentication server for MathHub written in Python and Django. 
+It expects to be run together with an NGINX as auth_server url. 
+
+It consists of only three routes:
+* `/admin/` -- a django admin instance to get free and cheap user management
+* `/admin/nginx/check` -- an auth_backend for nginx that return `HTTP 200` if a user is authorized and `HTTP 401` if not. 
+* `/admin/nginx/login` -- a route that redirects the user to a login page based on the `X-Original-URI` header. 
---
+++ mhadmin/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import reverse
+
+from urllib.parse import quote
+
+def authenticate(request):
+    """ Renders a login view that respects the "X-Original-URI" header. """
+    
+    url = "{}?next={}".format(
+        reverse('admin:login'), 
+        quote(request.META.setdefault("HTTP_X_ORIGINAL_URI", ""))
+    )
+    return HttpResponseRedirect(url)
+
+
+def check(request):
+    """ Checks if a user is authenticated """
+    if request.user and request.user.is_authenticated:
+        return HttpResponse("Authorized")
+    else:
+        return HttpResponse('Unauthorized', status=401)
+
